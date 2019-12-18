dee10c8b310902da9a402147259b28b9426a156b
Bonsior-yyc <740987012@qq.com>
2019-12-18 14:39:27 +0800

    new file:   main.c

---
+++ cmake-build-debug/CMakeFiles/StudentScoreSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentScoreSystem.dir/main.c.o"
+  "StudentScoreSystem"
+  "StudentScoreSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StudentScoreSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/StudentScoreSystem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for StudentScoreSystem.
+# This may be replaced when dependencies are built.
